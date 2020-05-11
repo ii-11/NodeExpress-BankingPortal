76d893efb5f3bb6015496ab0282a1285a7d4d2b5
ii-11 <ivanovski@vivaldi.net>
2020-05-11 22:40:33 +0200

    Create basic express app and add index view

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.set('views', path.join('src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'src', 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Index' });
+})
+
+app.listen(3000, () => {
+  console.log('PS Project Running on port 3000!');
+})
