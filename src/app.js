8e94f155832f87bdffe7650bb502823bcd4bced1
ii-11 <ivanovski@vivaldi.net>
2020-05-12 00:20:05 +0200

    Refactor routes to dedicates files

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const express = require('express');
-const { accounts, users, writeJSON } = require('./data')
+const { accounts, users, writeJSON } = require('./data');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
+
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -23,54 +26,8 @@ app.get('/profile', (req, res) => {
     });
 });
 
-app.get('/savings', (req, res) => {
-  res.render('account',
-    {
-      account: accounts.savings
-    });
-});
-app.get('/checking', (req, res) => {
-  res.render('account',
-    {
-      account: accounts.checking
-    });
-});
-app.get('/credit', (req, res) => {
-  res.render('account',
-    {
-      account: accounts.credit
-    });
-});
-
-app.get('/transfer', (req, res) => {
-  res.render('transfer');
-});
-app.post('/transfer', (req, res) => {
-  const from = accounts[req.body.from];
-  const to = accounts[req.body.to];
-  from.balance -= req.body.amount;
-  to.balance += parseInt(req.body.amount);
-  writeJSON();
-  res.render('transfer', {
-    message: 'Transfer Completed'
-  });
-});
-
-app.get('/payment', (req, res) => {
-  res.render('payment', {
-    account: accounts.credit
-  });
-});
-app.post('/payment', (req, res) => {
-  const credit = accounts['credit'];
-  credit.balance -= req.body.amount;
-  credit.available += parseInt(req.body.amount);
-  writeJSON();
-  res.render('payment', {
-    message: 'Payment Successful',
-    account: accounts.credit
-  });
-});
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.listen(3000, () => {
   console.log('PS Project Running on port 3000!');
